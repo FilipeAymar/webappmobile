6f8390826ca1c29b0c96ff240203e212a380eb4d
Filipe Aymar <130711610+FilipeAymar@users.noreply.github.com>
2024-04-15 21:05:18 -0300

    Update
    atualização no código da API + Hardware.

--- mapa/js/scripts.js
+++ mapa/js/scripts.js
@@ -1,27 +1,31 @@
-window.onload = function(){
-    var map = L.map('map').fitWorld();
+var map = L.map('map').setView([-8.047562, -34.877002], 13);
+        
+        // Define as coordenadas de Recife
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+        }).addTo(map);
 
-    map.locate({setView: true, maxZoom: 16});
+        // Adicione um marcador para a localização do usuário
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var userLat = position.coords.latitude;
+            var userLng = position.coords.longitude;
+            L.marker([userLat, userLng]).addTo(map)
+                .bindPopup('Sua Localização Atual')
+                .openPopup();
+        });
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-    }).addTo(map);
+        // Adicione marcadores para pontos pré-cadastrados em Recife
+        var points = [
+            { name: "AppleAssist - Madalena", latlng: [-8.05308, -34.9034] },
+            { name: "AppleAssist - Pina", latlng: [-8.10283, -34.88784] },
+            { name: "AppleAssist - Parnamirim", latlng: [-8.03115, -34.90576] }
+        ];
 
-    function onLocationFound(e) {
-        var radius = e.accuracy;
-    
-        L.marker(e.latlng).addTo(map)
-            .bindPopup("You are within " + radius + " meters from this point").openPopup();
-    
-        L.circle(e.latlng, radius).addTo(map);
-    }
-}
+        points.forEach(function (point) {
+            L.marker(point.latlng).addTo(map).bindPopup(point.name);
+        });
 
-map.on('locationfound', onLocationFound);
-
-function onLocationError(e) {
-    alert(e.message);
-}
-
-map.on('locationerror', onLocationError);
+        // Atualize o tamanho do mapa quando a janela for redimensionada
+        window.addEventListener('resize', function () {
+            map.invalidateSize();
+        });
